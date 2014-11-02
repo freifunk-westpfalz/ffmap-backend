99f4a97d54e1d7eed382fb341b678c00e98379de
Kokel <tobias@hachmer.de>
2014-11-02 11:28:12 +0100

    generalize firmware

--- mkmap.sh
+++ mkmap.sh
@@ -3,6 +3,7 @@
 set -e
 
 DEST=$1
+FIRMWARE=$2
 
 export PATH=/home/admin/bin:$PATH
 
@@ -10,4 +11,6 @@ export PATH=/home/admin/bin:$PATH
 
 cd "$(dirname "$0")"/
 
-./bat2nodes.py -a aliases-mz.json -m mzBAT -s /var/run/alfred-mz.sock -f 0.1 -d $DEST
+./bat2nodes.py -a aliases-mz.json -m mzBAT -s /var/run/alfred-mz.sock -f $FIRMWARE -d $DEST
+#./bat2nodes.py -a aliases-wi.json -m wiBAT -s /var/run/alfred-wi.sock -f $FIRMWARE -d $DEST
+
