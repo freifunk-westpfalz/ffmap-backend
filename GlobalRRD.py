578816db6ae7b378207dbc34b0a4f000dbdcd735
Kokel <tobias@hachmer.de>
2014-11-11 21:46:47 +0100

    pimp my GlobalRRD

--- GlobalRRD.py
+++ GlobalRRD.py
@@ -2,6 +2,9 @@ import os
 import subprocess
 from RRD import RRD, DS, RRA
 
+now = datetime.datetime.now().strftime('%d.%m.%Y %H:%M')
+prettynow = now.replace(":", "\:")
+
 class GlobalRRD(RRD):
     ds_list = [
         # Number of nodes available
@@ -11,8 +14,13 @@ class GlobalRRD(RRD):
     ]
     rra_list = [
         RRA('AVERAGE', 0.5, 1, 120),    #  2 hours of 1 minute samples
+        RRA('AVERAGE', 0.5, 60, 168),   #  7 days  of 1 hour   samples
         RRA('AVERAGE', 0.5, 60, 744),   # 31 days  of 1 hour   samples
         RRA('AVERAGE', 0.5, 1440, 1780),# ~5 years of 1 day    samples
+        RRA('MAX', 0.5, 1, 120),    #  2 hours of 1 minute samples
+        RRA('MAX', 0.5, 60, 168),   #  7 days  of 1 hour   samples
+        RRA('MAX', 0.5, 60, 744),   # 31 days  of 1 hour   samples
+        RRA('MAX', 0.5, 1440, 1780),# ~5 years of 1 day    samples
     ]
 
     def __init__(self, directory):
@@ -29,12 +37,13 @@ class GlobalRRD(RRD):
                 '-h' '400',
                 '--watermark=' 'Freifunk MWU',
                 'DEF:nodes=' + self.filename + ':nodes:AVERAGE',
-                'LINE1:nodes#F00:nodes    ',
+                'LINE2:nodes#0066FF:nodes    ',
                 'GPRINT:nodes:AVERAGE:avg\: %2.0lf',
                 'GPRINT:nodes:MAX:max\: %2.0lf\\l',
                 'DEF:clients=' + self.filename + ':clients:AVERAGE',
-                'LINE2:clients#00F:clients  ',
+                'AREA:clients#33CC33:clients  ',
                 'GPRINT:clients:AVERAGE:avg\: %2.0lf',
                 'GPRINT:clients:MAX:max\: %2.0lf\\l',
+                'COMMENT:Last Update\: ' + prettynow + '\\r',
         ]
         subprocess.check_output(args)
