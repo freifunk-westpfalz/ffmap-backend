5434540707ea7448be1941498dc35bf28f14f9c2
kokel <tobias@hachmer.de>
2014-12-15 15:21:32 +0100

    add cleanup script for state jsons

---
+++ cleanupstates.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', '--statefile', nargs='+', help='state files you want to clean up')
+
+args = parser.parse_args()
+options = vars(args)
+
+statefile = options['statefile']
+
+for s in range(len(statefile)):
+    obj  = json.load(open(statefile[s]))
+
+    for i in range(len(obj)):
+        if (obj[i]["name"] == "" and
+            obj[i]["uptime"] == ""):
+            obj.pop(i)
+            break
+
+    open(statefile[s], "w").write( json.dumps(obj, indent=4, separators=(',', ': ')) )
