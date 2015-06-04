479c0ddca0c93e615a83adf339fcb2f6b73b695e
kokel <tobias@hachmer.de>
2015-06-04 15:12:00 +0200

    add mkmap shell scripts

---
+++ mkmap-mz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+export PATH=/home/admin/bin:$PATH
+
+WORKDIR="/home/admin/clones/ffmap-backend-intern-mz"
+WWWDIRINTERN="/var/www/meshviewer-intern-mz"
+WWWDIREXTERN="/var/www/meshviewer-extern-mz"
+
+cd "$(dirname "$0")"/
+
+/usr/bin/python3 $WORKDIR/backend.py --with-rrd --prune 45 -m mzBAT:/var/run/alfred-mz.sock --vpn 02:00:0a:25:00:17 02:00:0a:25:00:05 02:00:0a:25:00:07 02:00:0a:25:00:d0 02:00:0a:25:00:e7 -d $WORKDIR/data/
+
+/usr/bin/jq '.nodes = (.nodes | with_entries(del(.value.nodeinfo.owner)))' < $WORKDIR/data/nodes.json > $WORKDIR/data/nodes-internet.json
+cp -r $WORKDIR/data/* $WWWDIRINTERN/build/data/
+
+cp $WORKDIR/data/nodes-internet.json $WWWDIREXTERN/build/data/nodes.json
+cp $WORKDIR/data/graph.json $WWWDIREXTERN/build/data/
+cp $WORKDIR/data/nodelist.json $WWWDIREXTERN/build/data/
+cp -r $WORKDIR/data/nodes $WWWDIREXTERN/build/data/
---
+++ mkmap-wi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+export PATH=/home/admin/bin:$PATH
+
+WORKDIR="/home/admin/clones/ffmap-backend-intern-wi"
+WWWDIRINTERN="/var/www/meshviewer-intern-wi"
+WWWDIREXTERN="/var/www/meshviewer-extern-wi"
+
+cd "$(dirname "$0")"/
+
+/usr/bin/python3 $WORKDIR/backend.py --with-rrd --prune 45 -m wiBAT:/var/run/alfred-wi.sock --vpn 02:00:0a:38:00:17 02:00:0a:38:00:05 02:00:0a:38:00:07 02:00:0a:38:00:d0 02:00:0a:38:00:e7 -d $WORKDIR/data/
+
+/usr/bin/jq '.nodes = (.nodes | with_entries(del(.value.nodeinfo.owner)))' < $WORKDIR/data/nodes.json > $WORKDIR/data/nodes-internet.json
+cp -r $WORKDIR/data/* $WWWDIRINTERN/build/data/
+
+cp $WORKDIR/data/nodes-internet.json $WWWDIREXTERN/build/data/nodes.json
+cp $WORKDIR/data/graph.json $WWWDIREXTERN/build/data/
+cp $WORKDIR/data/nodelist.json $WWWDIREXTERN/build/data/
+cp -r $WORKDIR/data/nodes $WWWDIREXTERN/build/data/
