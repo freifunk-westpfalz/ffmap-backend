68d355fb2eeaff52a77a54941f71c403fc17b7c1
Tobias Hachmer <tobias@hachmer.de>
2015-10-14 09:35:56 +0200

    remove ffmap-d3 nodes.json generation

--- mkmap-mz.sh
+++ mkmap-mz.sh
@@ -19,6 +19,3 @@ cp $WORKDIR/data/nodes-internet.json $WWWDIREXTERN/build/data/nodes.json
 cp $WORKDIR/data/graph.json $WWWDIREXTERN/build/data/
 cp $WORKDIR/data/nodelist.json $WWWDIREXTERN/build/data/
 cp -r $WORKDIR/data/nodes $WWWDIREXTERN/build/data/
-
-# Provide nodes.json in old ffmap-d3 format just for freifunk-karte.de
-/usr/bin/jq -n -f ffmap-d3.jq --argfile nodes data/nodes-internet.json --argfile graph data/graph.json > $WWWDIREXTERN/build/nodes.json
